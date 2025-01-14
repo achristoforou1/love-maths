f50d41f5313649d0b299301bf35adf686b7ffd22
Anthony Christoforou <a_christoforou1@yahoo.com>
2025-01-14 09:40:43 +0000

    display additiion questiom

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
@@ -18,26 +18,25 @@ document.addEventListener("DOMContentLoaded", function() {
     runGame("addition");
 
 });
+
 /**
- * The main game "loop", called when script is first loaded 
+ * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
 
-    //Creates two random numbers between 1 and 25
+    // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else {
-        alert(`Unknown game type: $(gameType)`);
-        throw `Unknown game type: $(gameType). Aborting!`;
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
     }
 
 }
-
-
 
 function checkAnswer() {
 
@@ -56,8 +55,9 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
+
     document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand1').textContent = operand2;
+    document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
     
 }
